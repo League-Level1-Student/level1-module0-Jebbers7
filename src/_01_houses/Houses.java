3ab32cd2aeb096674cb1d0468f7431a173e3ac44
student <student@ip-172-31-59-71.us-west-2.compute.internal>
2021-02-02 16:54:35 -0800

    Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,22 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
+		
+		Robot rob = new Robot();
+		rob.moveTo(50,500);
+		rob.penDown();
+		rob.setSpeed(50);
+		rob.move(100);
+		rob.turn(90);
+		rob.move(20);
+		rob.turn(90);
+		rob.move(100);
+		rob.setPenColor(0,255,0);
+		rob.turn(270);
+		rob.move(20);
 		
 	}
 }
